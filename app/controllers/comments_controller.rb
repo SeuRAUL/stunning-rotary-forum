e0735c81a18787b60f0753414758d13cd9b17fad
seuraul <seuraul.0@gmail.com>
2023-05-16 16:43:39 -0300

    Comments routes and controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :message, only: [:new, :create]
+
+  def new 
+    @comment = @message.comments.build
+  end
+
+  def create
+    @comment = @message.comments.create(comment_params)
+    @comment.user_id = current_user.user_id
+
+    if @comment.save 
+      redirect_to @message 
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def message 
+      @message = Message.find(params[:message_id])
+    end
+
+    def comment_params 
+      params.require(:comment).include(:content)
+    end
+end
